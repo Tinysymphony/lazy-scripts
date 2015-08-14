8c330835b3704d76e321839fcce8ef8467b79095
Tinysymphony <zjutiny@gmail.com>
2015-08-14 15:53:17 +0800

    fill syntax error

--- postgresql-xl/pg_register.sql
+++ postgresql-xl/pg_register.sql
@@ -10,19 +10,19 @@ drop node group gp10;
 drop node group gp11;
 drop node group gp12;
 
-create node pg_cd7 with(TYPE=coordinator, HOST=vag-node7, PORT=7002);
-create node pg_cd8 with(TYPE=coordinator, HOST=vag-node8, PORT=7002);
-create node pg_cd10 with(TYPE=coordinator, HOST=vag-node10, PORT=7002);
-create node pg_cd11 with(TYPE=coordinator, HOST=vag-node11, PORT=7002);
-create node pg_cd12 with(TYPE=coordinator, HOST=vag-node12, PORT=7002);
+create node pg_cd7 with(TYPE=coordinator, HOST='vag-node7', PORT=7002);
+create node pg_cd8 with(TYPE=coordinator, HOST='vag-node8', PORT=7002);
+create node pg_cd10 with(TYPE=coordinator, HOST='vag-node10', PORT=7002);
+create node pg_cd11 with(TYPE=coordinator, HOST='vag-node11', PORT=7002);
+create node pg_cd12 with(TYPE=coordinator, HOST='vag-node12', PORT=7002);
 
-create node pg_dn7 with(TYPE=datanode, HOST=vag-node7, PORT=7000, primary=false);
-create node pg_dn8 with(TYPE=datanode, HOST=vag-node8, PORT=7000, primary=false);
-create node pg_dn10 with(TYPE=datanode, HOST=vag-node10, PORT=7000, primary=false);
-create node pg_dn11 with(TYPE=datanode, HOST=vag-node11, PORT=7000, primary=false);
-create node pg_dn12 with(TYPE=datanode, HOST=vag-node12, PORT=7000, primary=false);
+create node pg_dn7 with(TYPE=datanode, HOST='vag-node7', PORT=7000, primary=false);
+create node pg_dn8 with(TYPE=datanode, HOST='vag-node8', PORT=7000, primary=false);
+create node pg_dn10 with(TYPE=datanode, HOST='vag-node10', PORT=7000, primary=false);
+create node pg_dn11 with(TYPE=datanode, HOST='vag-node11', PORT=7000, primary=false);
+create node pg_dn12 with(TYPE=datanode, HOST='vag-node12', PORT=7000, primary=false);
 
-alter node pg_dn11 with(TYPE=datanode, HOST=vag-node11, PORT=7000, primary=true);
+alter node pg_dn11 with(TYPE=datanode, HOST='vag-node11', PORT=7000, primary=true);
 
 create node group gp7 with(pg_dn7);
 create node group gp8 with(pg_dn8);
